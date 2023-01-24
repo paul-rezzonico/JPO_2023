9c98a40d28f38f8d164972bc171242b2a0187512
carlodrift <30211659+carlodrift@users.noreply.github.com>
2023-01-24 21:24:58 +0100

    Fix spelling

--- src/main/java/com/unilim/iut/info/jpo_2023/Main.java
+++ src/main/java/com/unilim/iut/info/jpo_2023/Main.java
@@ -17,10 +17,10 @@ public class Main {
         //initialisation de Nunu
         Nunu nunu = new Nunu(position, direction);
 
-        //Implémenter ci dessous les instruction nécéssaire afin que nunu arrive à la position(20,11)
-        //insipirez vous de la ligne suivante :
-        nunu.afficher();    //cette ligne permet d'afficher la position et la direction de nunu
-        nunu.avancer();     //cette ligne permet de faire avancer nunu d'une case dans la direction dans laquelle il est tourné
+        //Implémenter ci-dessous les instructions nécéssaires afin que Nunu arrive à la position (20, 11)
+        //inspirez-vous de la ligne suivante :
+        nunu.afficher();    //cette ligne permet d'afficher la position et la direction de Nunu
+        nunu.avancer();     //cette ligne permet de faire avancer Nunu d'une case dans la direction dans laquelle il est tourné
         nunu.afficher();
 
     }
